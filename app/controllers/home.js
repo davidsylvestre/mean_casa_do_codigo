3fbdec864a17a610734f0944ae61bb300569b7a1
unknown <dasylvestre@CONTAB_31.SMARAPD.COM.BR>
2016-01-18 14:09:50 -0200

    Commit inicial até o capitulo 4

---
+++ app/controllers/home.js
@@ -0,0 +1,12 @@
+module.exports = function() {
+    var controller = {};
+
+    controller.index = function(req, res) {
+        // retorna a pagina index.ejs
+        res.render('index', {
+            nome: 'Express'
+        });
+    };
+
+    return controller;
+}
